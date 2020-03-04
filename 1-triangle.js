1a9956faf7fec28c7698f519a9beac415a9da455
Dinu <dinoom97@gmail.com>
2020-03-04 12:35:08 +0200

    Fix description

--- 1-triangle.js
+++ 1-triangle.js
@@ -12,7 +12,8 @@
  ######
  #######
  *
- * It may be useful to know that you can find the length of a string by writing .length after it.let abc = "abc";
+ * It may be useful to know that you can find the length of a string by writing .length after it.
+ * let abc = "abc";
  * console.log(abc.length); → 3
  */
 
