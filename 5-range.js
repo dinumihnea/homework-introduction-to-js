5d7e86510c9b6e7ec5dcd334366e9576f96c5188
Dinu <dinoom97@gmail.com>
2020-03-04 12:38:36 +0200

    Fix tasks

--- 5-range.js
+++ 5-range.js
@@ -3,7 +3,7 @@
  *
  * to run code from this file use: npx nodemon 5-range
  *
- * 5.1 Write a range function that takes two arguments, start and end, and returns an array
+ * 5.1 Write a function that takes two arguments, start and end, and returns an array
  *    containing all the numbers from start up to (and including) end. Next, write a sum
  *    function that takes an array of numbers and returns the sum of these numbers.
  *
