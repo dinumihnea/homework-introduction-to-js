89f81365a4697677920ca570185585f670f648c5
TaVa <mihail.zvezdinskiy@gmail.com>
2020-03-05 17:17:10 +0200

    sixth task

--- 6-reverse.js
+++ 6-reverse.js
@@ -6,8 +6,11 @@
  * Write a function that takes an array as argument and produces a new array that has the same elements in the inverse order.
  *
  */
-
-// Your code here.
-
-// console.log(reverseArray(["A", "B", "C"]));
-// → ["C", "B", "A"];
+let reverseArray = (arr) => {
+    let newArr = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        newArr.push(arr[i]);
+    }
+    return newArr;
+};
+console.log(reverseArray(["A", "B", "C"]));
