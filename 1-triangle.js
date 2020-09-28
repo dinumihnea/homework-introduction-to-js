734b45d5813271ad60f3e0a7efcc494d23fa7172
Vlad Mardari <mardari1998@gmail.com>
2020-09-28 21:53:50 +0300

    triangle task added

--- 1-triangle.js
+++ 1-triangle.js
@@ -18,3 +18,14 @@
 
 // Your code here.
 
+function triangle(n){
+    for(let i = 0; i<n; i++){
+        let row = '';
+        for(let j = 0; j<i; j++){
+            row += '#';
+        }
+        console.log(row);
+    }
+}
+
+triangle(7);
