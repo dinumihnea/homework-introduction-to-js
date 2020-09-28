de5abce2cf83496563b16dbad7b0b59b38a833bb
Dinu <dinoom97@gmail.com>
2020-09-28 09:33:22 +0300

    Upgrade nodemon version

--- 1-triangle.js
+++ 1-triangle.js
@@ -12,9 +12,8 @@
  ######
  #######
  *
- * It may be useful to know that you can find the length of a string by writing .length after it.
- * let abc = "abc";
- * console.log(abc.length); → 3
+ * Try to find the most optimal way to do this.
+ *
  */
 
 // Your code here.
