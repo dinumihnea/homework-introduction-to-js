dec11c1bfc020ea95491e41acc8ee3db5a82f703
Vlad Mardari <mardari1998@gmail.com>
2020-09-28 21:58:39 +0300

    fizz-buzz task added

--- 2-fiz-buzz.js
+++ 2-fiz-buzz.js
@@ -13,3 +13,13 @@
  */
 
 // Your code here.
+
+function fizzBuzz(){
+    for(let i = 1; i<=100; i++){
+        if(i % 3 === 0) console.log('Fizz');
+        else if(i % 5 === 0) console.log('Buzz');
+        else console.log(i);
+    }
+}
+
+fizzBuzz();
