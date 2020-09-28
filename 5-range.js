d4f11c9710fef4e782af221e7b1c17f3e4875b0e
Vlad Mardari <mardari1998@gmail.com>
2020-09-28 22:30:53 +0300

    range task added

--- 5-range.js
+++ 5-range.js
@@ -16,10 +16,27 @@
  */
 
 // Your code here.
-//
+function range(start, end){
+    const arr = [];
+    for(let i = start; i<=end; i++){
+        arr.push(i);
+    }
+    return(arr);
+}
+function sum(arr){
+    //normal solution
+    // return arr.reduce((sum, currentElement) => sum + currentElement);
+
+    //caveman solution :D
+    let sum = 0;
+    arr.forEach(element => {
+        sum += element;
+    });
+    return sum;
+}
 // console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
-// console.log(sum(range(1, 10)));
+console.log(sum(range(1, 10)));
 // → 55
